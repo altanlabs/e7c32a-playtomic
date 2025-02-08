2e16363c1c0ded3a007809312bb0dd5cb536c84a
altanlabs <interfaces@altan.ai>
2025-02-08 19:24:06 +0000

    New commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
