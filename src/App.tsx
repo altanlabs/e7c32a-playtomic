7e892eaaf5a0c627a7c91b4c29d342605fdefd8b
altanlabs <interfaces@altan.ai>
2025-02-08 19:58:02 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,74 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
+import { Toaster } from "@/components/ui/sonner"
+import { ThemeProvider } from "@/theme/theme-provider"
+import Layout from '@/components/layout/Layout'
 import IndexPage from './pages'
+import TournamentsPage from './pages/tournaments'
+import TournamentDetailPage from './pages/tournaments/[id]'
+import CreateTournamentPage from './pages/tournaments/create'
+import TeamsPage from './pages/teams'
+import TeamDetailPage from './pages/teams/[id]'
+import CreateTeamPage from './pages/teams/create'
+import JoinAsPlayerPage from './pages/join-as-player'
+import RankingsPage from './pages/rankings'
+import NotFoundPage from './pages/NotFound'
+import InvitePlayersPage from './pages/invite-players'
+import InviteToPlayPage from './pages/invite-to-play'
+import JoinGameListPage from './pages/join-game'
+import JoinGameDetailPage from './pages/join-game/[id]'
+import PlayersPage from './pages/players'
+import CourtsPage from './pages/courts'
+import ClubsPage from './pages/clubs'
+import PublishCourtPage from './pages/clubs/publish'
 import BookingPage from './pages/booking'
+import BookingPaymentPage from './pages/booking/payment'
+import BookingConfirmationPage from './pages/booking/confirmation'
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/booking" element={<BookingPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            
+            {/* Booking Flow */}
+            <Route path="/booking" element={<BookingPage />} />
+            <Route path="/booking/payment" element={<BookingPaymentPage />} />
+            <Route path="/booking/confirmation" element={<BookingConfirmationPage />} />
+            
+            {/* Páginas principales del menú */}
+            <Route path="/players" element={<PlayersPage />} />
+            <Route path="/courts" element={<CourtsPage />} />
+            <Route path="/clubs" element={<ClubsPage />} />
+            <Route path="/clubs/publish" element={<PublishCourtPage />} />
+            <Route path="/tournaments" element={<TournamentsPage />} />
+            <Route path="/rankings" element={<RankingsPage />} />
+            
+            {/* Torneos */}
+            <Route path="/tournaments/:id" element={<TournamentDetailPage />} />
+            <Route path="/tournaments/create" element={<CreateTournamentPage />} />
+            <Route path="/publicar-evento" element={<CreateTournamentPage />} />
+            
+            {/* Equipos */}
+            <Route path="/teams" element={<TeamsPage />} />
+            <Route path="/teams/:id" element={<TeamDetailPage />} />
+            <Route path="/teams/create" element={<CreateTeamPage />} />
+            
+            {/* Jugadores y Partidos */}
+            <Route path="/join-as-player" element={<JoinAsPlayerPage />} />
+            <Route path="/invite-players" element={<InvitePlayersPage />} />
+            <Route path="/invite-to-play" element={<InviteToPlayPage />} />
+            <Route path="/join-game" element={<JoinGameListPage />} />
+            <Route path="/join-game/:id" element={<JoinGameDetailPage />} />
+            
+            {/* 404 */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Layout>
+        <Toaster />
+      </Router>
+    </ThemeProvider>
   )
 }
 
