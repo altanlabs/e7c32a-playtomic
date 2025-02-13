8be7444f801eaa45cfed1a789d049c0b205d3e66
altanlabs <interfaces@altan.ai>
2025-02-13 23:31:29 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { DatabaseProvider } from "@altanlabs/database";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from "@/lib/theme-provider";
-import { Routes } from "@/routes";
+import { ThemeProvider } from "@/theme/theme-provider";
+import { Routes } from "@/pages/_app";
 
 const config = {
   API_BASE_URL: "https://api.altan.ai/galaxia/hook/mo6VsG",
