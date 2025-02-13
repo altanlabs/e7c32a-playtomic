fc7ae848047a0ef47b015fe84a51771b3e7866cd
altanlabs <interfaces@altan.ai>
2025-02-13 23:27:08 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from "@/theme/theme-provider"
-import { Toaster } from "@/components/ui/sonner"
-import Layout from './layout'
+import { DatabaseProvider } from "@altanlabs/database";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Routes } from "@/routes";
 
-// Páginas principales
-import IndexPage from './pages'
-import PlayersPage from './pages/players'
-import ArosPage from './pages/courts'
-import ClubsPage from './pages/clubs'
-import TournamentsPage from './pages/tournaments/index'
-import JoinTournamentPage from './pages/tournaments/join'
-import RankingsPage from './pages/rankings'
-import PlayerProfilePage from './pages/perfil/[id]'
-
-// Páginas de funcionalidad
-import BookingPage from './pages/booking'
-import BookingPaymentPage from './pages/booking/payment'
-import BookingConfirmationPage from './pages/booking/confirmation'
-
-// Páginas de creación y gestión
-import PublishAroPage from './pages/clubs/publish'
-import CreateTournamentPage from './pages/tournaments/create'
-import JoinAsPlayerPage from './pages/join-as-player'
-import InviteToPlayPage from './pages/invite-to-play'
-
-// Página 404
-import NotFoundPage from './pages/NotFound'
+const config = {
+  API_BASE_URL: "https://api.altan.ai/galaxia/hook/mo6VsG",
+  SAMPLE_TABLES: {
+    waitlist: "4ff1558e-4247-40d3-b4d5-3ce2d4cc5616"
+  }
+};
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Layout>
-          <Routes>
-            {/* Páginas principales */}
-            <Route path="/" element={<IndexPage />} />
-            <Route path="/players" element={<PlayersPage />} />
-            <Route path="/aros" element={<ArosPage />} />
-            <Route path="/clubs" element={<ClubsPage />} />
-            <Route path="/tournaments" element={<TournamentsPage />} />
-            <Route path="/tournaments/join" element={<JoinTournamentPage />} />
-            <Route path="/rankings" element={<RankingsPage />} />
-            <Route path="/perfil/:id" element={<PlayerProfilePage />} />
-            
-            {/* Flujo de reserva */}
-            <Route path="/booking" element={<BookingPage />} />
-            <Route path="/booking/payment" element={<BookingPaymentPage />} />
-            <Route path="/booking/confirmation" element={<BookingConfirmationPage />} />
-            
-            {/* Páginas de creación y gestión */}
-            <Route path="/clubs/publish" element={<PublishAroPage />} />
-            <Route path="/tournaments/create" element={<CreateTournamentPage />} />
-            <Route path="/join-as-player" element={<JoinAsPlayerPage />} />
-            <Route path="/invite-to-play" element={<InviteToPlayPage />} />
-            
-            {/* 404 */}
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Layout>
-        <Toaster />
-      </Router>
-    </ThemeProvider>
-  )
+    <DatabaseProvider config={config}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Router>
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    </DatabaseProvider>
+  );
 }
 
-export default App
+export default App;
