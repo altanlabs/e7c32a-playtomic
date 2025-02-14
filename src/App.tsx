5f8ffbbb58efb0e5b224ea40dc173b9dafae087f
altanlabs <interfaces@altan.ai>
2025-02-14 01:24:13 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/theme/theme-provider"
-import { Provider } from "react-redux"
-import { store } from "@altanlabs/database"
+import { DatabaseProvider } from "@altanlabs/database"
+
+const config = {
+  API_BASE_URL: "https://api.altan.ai/platform/database",
+  SAMPLE_TABLES: {}  // We'll add tables when needed
+}
 
 export default function App() {
   return (
-    <Provider store={store}>
+    <DatabaseProvider config={config}>
       <ThemeProvider>
         <RouterProvider router={router} />
         <Toaster />
       </ThemeProvider>
-    </Provider>
+    </DatabaseProvider>
   )
 }
