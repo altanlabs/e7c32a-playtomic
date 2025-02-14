eee66dfe33a4cbd96a7a1fa410feb6e5da199c0f
altanlabs <interfaces@altan.ai>
2025-02-14 01:07:54 +0000

    New commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,37 @@
-import { Route, Routes as RouterRoutes } from "react-router-dom";
-import IndexPage from "./pages/index";
-import NotFoundPage from "./pages/NotFound";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layout";
+
+// Pages
+import HomePage from "./pages/index";
 import PlayersPage from "./pages/players";
+import CourtsPage from "./pages/courts";
 import TournamentsPage from "./pages/tournaments";
 import RankingsPage from "./pages/rankings";
+import NotFoundPage from "./pages/NotFound";
 
-export function Routes() {
-  return (
-    <RouterRoutes>
-      <Route path="/" element={<IndexPage />} />
-      <Route path="/players" element={<PlayersPage />} />
-      <Route path="/aros" element={<PlayersPage />} />
-      <Route path="/tournaments" element={<TournamentsPage />} />
-      <Route path="/rankings" element={<RankingsPage />} />
-      <Route path="/rules" element={<NotFoundPage />} />
-      <Route path="/terms" element={<NotFoundPage />} />
-      <Route path="/contact" element={<NotFoundPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </RouterRoutes>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/players",
+    element: <PlayersPage />,
+  },
+  {
+    path: "/courts",
+    element: <CourtsPage />,
+  },
+  {
+    path: "/tournaments",
+    element: <TournamentsPage />,
+  },
+  {
+    path: "/rankings",
+    element: <RankingsPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
