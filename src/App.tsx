c8f099ec5d478530b8776e13d863ce42982143a1
altanlabs <interfaces@altan.ai>
2025-02-14 01:20:25 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/theme/theme-provider"
+import { Provider } from "react-redux"
+import { store } from "@altanlabs/database"
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
+    </Provider>
   )
 }
