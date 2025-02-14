9ba3fd2b139f7f7e56afe523b8a831ae785d0adb
altanlabs <interfaces@altan.ai>
2025-02-14 01:43:00 +0000

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { RouterProvider } from "react-router-dom"
-import { router } from "./routes"
-import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "@/theme/theme-provider"
+import { DatabaseProvider } from "@altanlabs/database"
+import { BrowserRouter as Router } from "react-router-dom"
+import { ThemeProvider } from "./theme/theme-provider"
+import { Toaster } from "sonner"
+import { Routes } from "./routes"
 
-export default function App() {
+const config = {
+  API_BASE_URL: "https://api.altan.ai/galaxia/hook/mo6VsG",
+  SAMPLE_TABLES: {
+    waitlist: "d627090e-a358-4b0b-ad60-22af8b0485a1"  // Correct table ID
+  }
+}
+
+function App() {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </ThemeProvider>
+    <DatabaseProvider config={config}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Router>
+          <Routes />
+          <Toaster />
+        </Router>
+      </ThemeProvider>
+    </DatabaseProvider>
   )
 }
+
+export default App
