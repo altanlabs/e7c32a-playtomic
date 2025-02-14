28eb22adcd2b2f4c5a44655308e6f793ed42eaf9
altanlabs <interfaces@altan.ai>
2025-02-14 01:43:22 +0000

    New commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,44 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "./layout";
-import RootBoundary from "./components/errors/RootBoundary";
+import { Route, Routes as RouterRoutes } from "react-router-dom"
+import Layout from "./layout"
 
 // Pages
-import HomePage from "./pages/index";
-import PlayersPage from "./pages/players";
-import CourtsPage from "./pages/courts";
-import TournamentsPage from "./pages/tournaments";
-import RankingsPage from "./pages/rankings";
-import NotFoundPage from "./pages/NotFound";
+import HomePage from "./pages/index"
+import PlayersPage from "./pages/players"
+import CourtsPage from "./pages/courts"
+import TournamentsPage from "./pages/tournaments"
+import RankingsPage from "./pages/rankings"
+import NotFoundPage from "./pages/NotFound"
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-    errorElement: <RootBoundary />,
-  },
-  {
-    path: "/players",
-    element: <PlayersPage />,
-    errorElement: <RootBoundary />,
-  },
-  {
-    path: "/courts",
-    element: <CourtsPage />,
-    errorElement: <RootBoundary />,
-  },
-  {
-    path: "/tournaments",
-    element: <TournamentsPage />,
-    errorElement: <RootBoundary />,
-  },
-  {
-    path: "/rankings",
-    element: <RankingsPage />,
-    errorElement: <RootBoundary />,
-  },
-  {
-    path: "*",
-    element: <NotFoundPage />,
-    errorElement: <RootBoundary />,
-  },
-]);
+export function Routes() {
+  return (
+    <RouterRoutes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/players" element={<PlayersPage />} />
+      <Route path="/courts" element={<CourtsPage />} />
+      <Route path="/tournaments" element={<TournamentsPage />} />
+      <Route path="/rankings" element={<RankingsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </RouterRoutes>
+  )
+}
